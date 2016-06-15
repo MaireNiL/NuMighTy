65ed5e0f4c1a85656d22c1de91d503d3bec9fb55
MaireNiL <ml28@sanger.ac.uk>
2016-06-15 10:51:32 +0100

    Update 1_getTranslocated_reads.sh

--- orig/1_getTranslocated_reads.sh
+++ orig/1_getTranslocated_reads.sh
@@ -1,3 +1,3 @@
-echo 000 ; samtools view -F 8 -q 10  sample000.bam | awk '$7!="="' | sort -k7,7n -k8,8n > sample000_tr_mq10.txt  &
-echo 001 ; samtools view -F 8 -q 10  sample001.bam | awk '$7!="="' | sort -k7,7n -k8,8n > sample001_tr_mq10.txt  &
-echo 002 ; samtools view -F 8 -q 10  sample002.bam | awk '$7!="="' | sort -k7,7n -k8,8n > sample002_tr_mq10.txt  &
+echo sample000 ; samtools view -F 8 -q 10  sample000.bam | awk '$7!="="' | sort -k7,7n -k8,8n > sample000_tr_mq10.txt  &
+echo sample001 ; samtools view -F 8 -q 10  sample001.bam | awk '$7!="="' | sort -k7,7n -k8,8n > sample001_tr_mq10.txt  &
+echo sample002 ; samtools view -F 8 -q 10  sample002.bam | awk '$7!="="' | sort -k7,7n -k8,8n > sample002_tr_mq10.txt  &
