48fd247399b43a7a9e8695560e276c6f02112189
MaireNiL <ml28@sanger.ac.uk>
2016-06-15 11:23:27 +0100

    Update 2_run_filler.sh

--- orig/brass/2_run_filler.sh
+++ orig/brass/2_run_filler.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo r01_samplenumber.BRASS2num.txt; python 02_fillerDNA_fastq.py r01_samplenumber.BRASS2num.txt
+echo r01_samplenumber.BRASS2num.txt; python 2_fillerDNA_fastq.py r01_samplenumber.BRASS2num.txt
