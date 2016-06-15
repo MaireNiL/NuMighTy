44e32a618c8dabd79317800d2e68b2782e87d501
MaireNiL <ml28@sanger.ac.uk>
2016-06-15 11:02:38 +0100

    Update 1_runBRASSII_1MB.sh

--- orig/brass/1_runBRASSII_1MB.sh
+++ orig/brass/1_runBRASSII_1MB.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp  `pipelineResult -db live -s samplenumber -p 814 -t 142`  samplenumber_brass2.tab ; python 01_getBRASSII_results_gt1mb.py samplenumber_brass2.tab
+cp  `pipelineResult -db live -s samplenumber -p 814 -t 142`  samplenumber_brass2.tab ; python 1_getBRASSII_results_gt1mb.py samplenumber_brass2.tab
