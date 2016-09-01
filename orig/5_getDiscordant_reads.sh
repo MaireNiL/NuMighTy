39fe0bffbc70af7d44b90fc8ac462b1a92a02c0d
MaireNiL <ml28@sanger.ac.uk>
2016-09-01 12:32:01 +0100

    Delete 5_getDiscordant_reads.sh

--- orig/5_getDiscordant_reads.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo sample00_MT.bam; samtools view -F 14 sample00_MT.bam | awk '$7=="=" && $2!=145 && $2!=81 && $2!=161 && $2!=97' | wc -l
-echo sample01_MT.bam; samtools view -F 14 sample01_MT.bam | awk '$7=="=" && $2!=145 && $2!=81 && $2!=161 && $2!=97' | wc -l
