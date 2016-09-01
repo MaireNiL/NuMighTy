ea14d10323831c31340395ca10e1fe7d1af5c4ff
MaireNiL <ml28@sanger.ac.uk>
2016-09-01 12:30:55 +0100

    Delete 1_getTranslocated_circos.sh

--- orig/1_getTranslocated_circos.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo 0 ; samtools view -F 8 sample0_MT.bam | awk '$7!="=" && $7!="Y"' | sort -k7,7n -k8,8n > sample0_MTtr.txt ; python 2_forCircos.py sample0_MTtr.txt
-echo 1 ; samtools view -F 8 sample1.bam | awk '$7!="=" && $7!="Y"' | sort -k7,7n -k8,8n > sample1_MTtr.txt ; python 2_forCircos.py sample1_MTtr.txt
