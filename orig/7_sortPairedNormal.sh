c19c0198bb2155a8f666f7b0e18e5c40fa7829da
MaireNiL <ml28@sanger.ac.uk>
2016-09-01 12:32:18 +0100

    Delete 7_sortPairedNormal.sh

--- orig/7_sortPairedNormal.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sort -k7,7n -k8,8n -k9,9n	hbam00_tr.clusters_ori.ctr 	>	hbam00_tr.clusters_ori_s.ctr 
-sort -k7,7n -k8,8n -k9,9n	hbam01_tr.clusters_ori.ctr 	>	hbam01_tr.clusters_ori_s.ctr 
